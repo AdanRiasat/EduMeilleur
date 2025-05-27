4aaec04cee96de9173ae86053d6ca14a4d820da4
Adan Riasat <6194262@cegepmontpetit.ca>
2025-05-26 23:09:23 -0400

    FCT NavBar structure

---
+++ eduMeilleur/src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NgbCarousel],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
