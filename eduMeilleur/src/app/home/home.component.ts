79785de27230fae1dd150b01cfbdfb8860dc4648
Adan Riasat <6194262@cegepmontpetit.ca>
2025-06-08 14:48:10 -0400

    FCT Home page layout

--- eduMeilleur/src/app/home/home.component.ts
+++ eduMeilleur/src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { ImageSliderComponent } from '../image-slider/image-slider.component';
 import { SujetService } from '../services/sujet.service';
 import { DisplaySujet } from '../models/displaySujet';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgbCarouselModule, CommonModule, ImageSliderComponent],
+  imports: [NgbCarouselModule, CommonModule, ImageSliderComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
