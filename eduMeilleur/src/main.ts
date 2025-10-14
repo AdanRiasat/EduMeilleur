907540816e3af267e86b51e67c7206868873df24
Adan Riasat <6194262@cegepmontpetit.ca>
2025-10-14 01:21:59 -0400

    FCT Header layout for mobile with hamburger drawer

--- eduMeilleur/src/main.ts
+++ eduMeilleur/src/main.ts
@@ -3,8 +3,8 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import 'hammerjs';
 import { register } from 'swiper/element/bundle';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
 
-  register()
+register();
