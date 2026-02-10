5f8d020331a2fe66075e8f29beb764abe8d7359c
Adan Riasat <6194262@cegepmontpetit.ca>
2026-02-09 23:10:52 -0500

    FCT Removed header and footer on login and register

--- eduMeilleur/src/app/app.component.ts
+++ eduMeilleur/src/app/app.component.ts
@@ -1,21 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, effect, OnInit } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ModalComponent } from './components/modal/modal.component';
 import { GlobalService } from './services/global.service';
 import { ModalService } from './services/modal.service';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, CommonModule, NgxSpinnerModule, ModalComponent, SidebarComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, RouterModule, CommonModule, NgxSpinnerModule, ModalComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  constructor(public modalService: ModalService, public global: GlobalService) {}
+  showMainOnly: boolean = false
+  mainOnlyUrls: string[] = ['/login', '/signup']
+
+  constructor(public modalService: ModalService, public global: GlobalService, public router: Router) {
+   this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.showMainOnly = this.mainOnlyUrls.includes(event.urlAfterRedirects);
+      });
+  }
 }
