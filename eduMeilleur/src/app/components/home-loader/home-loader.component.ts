c7f19b5f273fb79af0fd7fa5a2816c5542a3beaf
AdanRiasat <2ariasat@gmail.com>
2026-06-05 00:42:12 -0400

    FCT: home-loader component for loading screen on home

---
+++ eduMeilleur/src/app/components/home-loader/home-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home-loader',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-loader.component.html',
+  styleUrl: './home-loader.component.css',
+})
+export class HomeLoaderComponent {
+  @Input() visible: boolean = true;
+}
