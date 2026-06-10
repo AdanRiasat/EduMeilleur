365c3abcfa8a32628108d33a3e6e9883640972bc
AdanRiasat <2ariasat@gmail.com>
2026-06-10 04:01:11 -0400

    FIX: edit-profile uses userService.getProfile with loading and error handling

--- eduMeilleur/src/app/pages/edit-profile/edit-profile.component.ts
+++ eduMeilleur/src/app/pages/edit-profile/edit-profile.component.ts
@@ -6,90 +6,109 @@ import { UserService } from '../../services/user.service';
 import { Profile } from '../../models/profile';
 import { SpinnerService } from '../../services/spinner.service';
 import { GlobalService } from '../../services/global.service';
+import { ModalService } from '../../services/modal.service';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './edit-profile.component.html',
-  styleUrl: './edit-profile.component.css'
+  styleUrl: './edit-profile.component.css',
 })
 export class EditProfileComponent implements OnInit {
-  school: string = ""
-  schoolYear: string = ""
-  username: string = ""
-  bio: string = ""
-  email: string = ""
+  school: string = '';
+  schoolYear: string = '';
+  username: string = '';
+  bio: string = '';
+  email: string = '';
 
   timestamp: number = Date.now();
 
+  file: File | null = null;
 
-  file: File | null = null
+  profile: Profile | null = null;
 
-  profile: Profile | null = null
+  @ViewChild('profileImage', { static: false }) profileImage?: ElementRef<HTMLImageElement>;
+  @ViewChild('fileInput', { static: false }) fileInput?: ElementRef<HTMLInputElement>;
 
-  @ViewChild('profileImage', {static: false}) profileImage ?: ElementRef<HTMLImageElement>
-  @ViewChild('fileInput', {static: false}) fileInput ?: ElementRef<HTMLInputElement>
+  constructor(
+    public route: Router,
+    public userService: UserService,
+    public spinner: SpinnerService,
+    public global: GlobalService,
+    public modalService: ModalService,
+    public toastService: ToastService,
+  ) {}
 
-  constructor(public route: Router, public userService: UserService, public spinner: SpinnerService, public global: GlobalService) {}
-  
-  ngOnInit() {
-    let profileStringData = localStorage.getItem("profile")  
-    if (profileStringData != null){
-      this.profile = JSON.parse(profileStringData)
-      this.username = this.profile!!.username
-      this.bio = this.profile!!.bio ?? ""
-      this.email = this.profile!!.email
+  async ngOnInit() {
+    this.spinner.show();
+    this.profile = await this.userService.getProfile();
+    this.username = this.profile!.username;
+    this.bio = this.profile.bio ?? '';
+    this.email = this.profile!.email;
 
-      if (this.profile?.schoolId == null){
-        this.school == ""
-      } else {
-        this.school = this.profile.schoolId.toString()
-      }
-
-      if (this.profile?.schoolYear == null){
-        this.schoolYear == ""
-      } else {
-        this.schoolYear = this.profile.schoolYear.toString()
-      }
+    if (this.profile?.schoolId == null) {
+      this.school == '';
+    } else {
+      this.school = this.profile.schoolId.toString();
     }
-  }
-  
-  triggerFileInput(): void {
-    if (this.fileInput != undefined)
-      this.fileInput.nativeElement.click();
+
+    if (this.profile?.schoolYear == null) {
+      this.schoolYear == '';
+    } else {
+      this.schoolYear = this.profile.schoolYear.toString();
+    }
+    this.spinner.hide();
   }
 
-  onFileSelected(event: Event){
-   let input = event.target as HTMLInputElement
-   if (input.files && input.files.length > 0){
-    this.file = input.files[0]
-    
+  triggerFileInput(): void {
+    if (this.fileInput != undefined) this.fileInput.nativeElement.click();
+  }
+
+  onFileSelected(event: Event) {
+    let input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.file = input.files[0];
+
       let reader = new FileReader();
       reader.onload = () => {
-        if (this.profileImage != undefined){
+        if (this.profileImage != undefined) {
           this.profileImage.nativeElement.src = reader.result as string;
         }
       };
       reader.readAsDataURL(this.file);
-      
-   }
+    }
   }
 
   //TODO Error messages
-  async saveChanges(){
-    this.spinner.show()
-    let formData = new FormData()
+  async saveChanges() {
+    this.spinner.show();
+    let formData = new FormData();
 
-    if (this.file != null){
-      formData.append("pfp", this.file)
+    try {
+      if (this.file != null) {
+        formData.append('pfp', this.file);
+      }
+      formData.append('email', this.email);
+      formData.append('bio', this.bio);
+      formData.append('school', this.school);
+      formData.append('schoolYear', this.schoolYear.toString());
+
+      await this.userService.editProfile(formData);
+      this.route.navigate(['/profile']);
+    } catch (error: any) {
+      if (error.error) {
+        if (error.status === 0 || error.status === 500) {
+          this.modalService.openErrorModal(() => this.saveChanges());
+          this.spinner.hide();
+          return;
+        } else {
+          this.toastService.error(error.error[0].description);
+        }
+      }
     }
-    formData.append("email", this.email)
-    formData.append("bio", this.bio)
-    formData.append("school", this.school)
-    formData.append("schoolYear", this.schoolYear.toString())
 
-    await this.userService.editProfile(formData)
-    this.route.navigate(['/profile'])
+    this.spinner.hide();
   }
 }
